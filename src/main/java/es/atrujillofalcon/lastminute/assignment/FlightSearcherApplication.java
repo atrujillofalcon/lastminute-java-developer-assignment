19515ed81157784318a2a68a039d6380606b05b9
atrujillo <atrujillo@paradigmadigital.com>
2019-05-07 17:00:40 +0200

    Creating spring boot application

---
+++ src/main/java/es/atrujillofalcon/lastminute/assignment/FlightSearcherApplication.java
@@ -0,0 +1,16 @@
+package es.atrujillofalcon.lastminute.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Arnaldo Trujillo
+ */
+@SpringBootApplication
+public class FlightSearcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightSearcherApplication.class, args);
+    }
+
+}
